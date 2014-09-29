38d1a730ce03b76dc04acb087b37cc5be83a3bb1
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-09-29 10:58:35 -0400

    Initial tests.

--- test/BrokenGradingSystemTest.java
+++ test/BrokenGradingSystemTest.java
@@ -1,17 +1,14 @@
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class BrokenGradingSystemTest {
 
 	private BrokenGradingSystem system;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		system = new BrokenGradingSystem();
@@ -27,28 +24,29 @@ public class BrokenGradingSystemTest {
 		system.registerGrade("dominic", 80);
 		system.registerGrade("dominic", 100);
 		system.registerGrade("david", 90);
-		
+
 		List<Integer> grades = system.getGrades("dominic");
-		
-		assertEquals("There should be 2 registered grades for dominic.",
-				2, grades.size());
+
+		assertEquals("There should be 2 registered grades for dominic.", 2,
+				grades.size());
 		assertEquals(80, (int) grades.get(0));
 		assertEquals(100, (int) grades.get(1));
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
+
+	@Test(expected = IllegalArgumentException.class)
 	public void testRegisterGradeWithNegativeThrows() {
 		system.registerGrade("dominic", -1);
 	}
-	
+
 	@Test
 	public void testRegisterGradeWithOver100() {
 		try {
 			system.registerGrade("dominic", 101);
 			fail();
-		} catch (Exception e) {}
+		} catch (Exception e) {
+		}
 	}
-	
+
 	@Test
 	public void testGetAverage() {
 		system.registerGrade("dominic", 25);
